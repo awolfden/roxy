b1702d0b75669c8b7c9271d4539ebe10b4e9fa3e
Adam Wolfman <47123183+awolfden@users.noreply.github.com>
2019-07-01 21:41:38 -0600

    Update precache-manifest.da8b33e40ac084f5cd11718f813ebf2c.js

--- precache-manifest.da8b33e40ac084f5cd11718f813ebf2c.js
+++ precache-manifest.da8b33e40ac084f5cd11718f813ebf2c.js
@@ -25,15 +25,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "1a58f436b67dd8f67a2f2abed7e5559a",
-    "url": "/static/media/gramophone.1a58f436.png"
+    "url": "/media/gramophone.1a58f436.png"
   },
   {
     "revision": "cf59c5b73d5ef38e67320ebcfc5c1d56",
-    "url": "/roxy/static/media/gramophone_logo_white.cf59c5b7.png"
+    "url": "/static/media/gramophone_logo_white.cf59c5b7.png"
   },
   {
     "revision": "05a36bd76737cd63153f43a400785cbf",
-    "url": "/roxy/static/media/openformusic.05a36bd7.png"
+    "url": "/static/media/openformusic.05a36bd7.png"
   },
   {
     "revision": "a75d4046144fb33a6d70ee586eaaeab3",

a4fd05d99e34611a90ee5dd47d980b2f86a11475
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-07-17 13:52:47 -0600

    added loader animation

--- roxy/src/App.js
+++ roxy/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './Navigation/Navigation';
 import Home from './Home/Home';
@@ -7,23 +7,47 @@ import Calendars from './Calendars/Calendars';
 import FoodMenu from './FoodMenu/FoodMenu';
 import Contact from './Contact/Contact';
 import Shop from './Shop/Shop';
+import Loader from './Loader/Loader';
 
 
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      showLoader : true
+    }
+  }
+  
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        showLoader: false
+      })
+    }, 2000);
+  }
 
-        <Navigation/>
-        <Home/>
-        <OpenForMusic/>
-        <Calendars/>
-        <FoodMenu/>
-        <Contact/>
-        {/* <Shop/> */}
 
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+          
+          
+         {this.state.showLoader ? <Loader/> : null}
+         {this.state.showLoader ? null : <Navigation/>}
+         {this.state.showLoader ? null : <Home/>}
+         {this.state.showLoader ? null : <OpenForMusic/>}
+         {this.state.showLoader ? null : <Calendars/>}
+         {this.state.showLoader ? null : <Navigation/>}
+         {this.state.showLoader ? null : <FoodMenu/>}
+         {this.state.showLoader ? null : <Contact/>}
+         
+         {/* <Shop/> */}
+  
+      </div>
+    )
+  }
+  
 }
 
 export default App;

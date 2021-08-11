deb177b4950690bcb1ea1fd84d642a7e295bb7e4
Adam Wolfman <adamwolfman@Adams-MBP-2.domain>
2021-08-10 20:04:05 -0600

    updated gift card link

--- roxy/src/HolidayGiftCard/HolidayGiftCard.jsx
+++ roxy/src/HolidayGiftCard/HolidayGiftCard.jsx
@@ -7,7 +7,7 @@ class HolidayGiftCard extends Component {
         return(
             <div id="holidayGiftCard">
                 <div className='flex-container image'>
-                    <a href="https://www.toasttab.com/roxy-554-s-broadway/giftcards" target="_blank" rel="noopener noreferrer">
+                    <a href="https://roxyonbroadway.thundertix.com/gifts/new" target="_blank" rel="noopener noreferrer">
                         <img src={require('../images/summer_banner.png')} alt="click for holiday gift card"/>
                     </a>
                 </div>

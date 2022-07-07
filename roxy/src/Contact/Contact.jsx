4b6bb6a60ae8f6f5fab8f379c8ebe9cfba9b8d30
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2022-07-07 16:59:04 -0600

    Update reservation button source

--- roxy/src/Contact/Contact.jsx
+++ roxy/src/Contact/Contact.jsx
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import contactRoxy from '../images/contact-roxy-logo.png';
 
 class Contact extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            
+
             locations: [
                 {
                     city: 'Broadway',
@@ -17,8 +17,7 @@ class Contact extends Component {
                     linkemail: 'mailto:info@broadwayroxy.com',
                     booking: 'booking@broadwayroxy.com',
                     linkbooking: 'mailto:booking@broadwayroxy.com',
-                    reservations: 'https://www.yelp.com/reservations/broadway-roxy-denver-2?from_reserve_now=1',
-                    map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1492.7684190468708!2d-104.98834954697357!3d39.70658113242791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c7f3607206573%3A0x68e9a9bb0301bbb8!2sBroadway+Roxy!5e0!3m2!1sen!2sus!4v1561927059341!5m2!1sen!2sus'
+                    reservations: 'https://www.opentable.com/r/roxy-on-broadway-reservations-denver?restref=1240837&lang=en-US&ot_source=Restaurant%20website'
                 },
                 {
                     city: 'Encinitas',
@@ -30,25 +29,24 @@ class Contact extends Component {
                     linkemail: 'mailto:info@roxyencinitas.com',
                     booking: 'booking@roxyencinitas.com',
                     linkbooking: 'mailto:booking@broadwayroxy.com',
-                    //reservations: 'https://www.yelp.com/reservations/the-roxy-encinitas-encinitas-2?from_reserve_now=1',
                     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1672.2023423089427!2d-117.29443066941882!3d33.04581115499584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc0c42515d331d%3A0xaccec1e35e94b68d!2sThe+Roxy+Encinitas!5e0!3m2!1sen!2sus!4v1561927336268!5m2!1sen!2sus'
                 }
             ]
         }
     }
 
-    render(){
+    render() {
         const barLocations = this.state.locations.map((bar, index) => {
-            return(
+            return (
                 <div key={index} id={index}>
                     <div className='flexContainer'>
                         <div className='info'>
                             <a href={bar.website} target="_blank" rel="noopener noreferrer"><h3>{bar.city}</h3></a>
                             <p>{bar.address1}</p>
-                            <p>{bar.address2}</p><br/>
+                            <p>{bar.address2}</p><br />
                             <p>Phone</p>
                             <p className='contactLink'>{bar.phone}</p>
-                            
+
                             <a href={bar.linkemail} className='emailLink'>
                                 <p className='contactTitle'>Email</p>
                                 <p className='contactLink'>{bar.email}</p>
@@ -60,17 +58,17 @@ class Contact extends Component {
                             </a>
                             {bar.reservations ? <a href={bar.reservations} target="_blank" rel="noopener noreferrer"><button>Reserve Now</button></a> : null}
                         </div>
-                        <div className="map"><iframe src={bar.map} title={index}width={500} height={350} frameBorder={0} style={{border:0, borderRadius: "10px", opacity:0.9}} allowFullScreen></iframe></div>
+                        <div className="map"><iframe src={bar.map} title={index} width={500} height={350} frameBorder={0} style={{ border: 0, borderRadius: "10px", opacity: 0.9 }} allowFullScreen></iframe></div>
                     </div>
-                    
+
                 </div>
             )
         })
 
-        return(
+        return (
             <div id="contact" className="contact">
                 <header className="App-header">
-                        <img src={contactRoxy} className="shop-logo" alt="logo" />
+                    <img src={contactRoxy} className="shop-logo" alt="logo" />
                 </header>
                 <div className='flexColumn'>
                     {barLocations}
